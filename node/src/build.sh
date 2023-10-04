2e31b47555f169077b795bed543368a310dd5970
UtopiaEH <utopiaeh01@gmail.com>
2023-10-04 14:55:02 +0300

    Fixed bash file for node

---
+++ node/src/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PROTO_DES_DIR=../../proto/generated
+PROTO_SRC_DIR=../../proto
+
+# Get the absolute path to grpc_tools_node_protoc_plugin
+
+GRPC_TOOLS_NODE_PROTOC_PLUGIN_JS_PATH=../node_modules/.bin/grpc_tools_node_protoc_plugin
+GRPC_TOOLS_NODE_PROTOC_PLUGIN_TS_PATH=../node_modules/.bin/protoc-gen-ts
+
+# Generate JavaScript code
+npx grpc_tools_node_protoc \
+    --js_out=import_style=commonjs,binary:${PROTO_DES_DIR} \
+    --grpc_out=grpc_js:${PROTO_DES_DIR} \
+    --plugin=protoc-gen-grpc=${GRPC_TOOLS_NODE_PROTOC_PLUGIN_JS_PATH} \
+    -I ${PROTO_SRC_DIR} \
+    ${PROTO_SRC_DIR}/*.proto
+
+# Generate TypeScript code (d.ts)
+npx grpc_tools_node_protoc \
+    --plugin=protoc-gen-grpc=${GRPC_TOOLS_NODE_PROTOC_PLUGIN_TS_PATH} \
+    --ts_out=${PROTO_DES_DIR} \
+    -I ${PROTO_SRC_DIR} \
+    ${PROTO_SRC_DIR}/*.proto
